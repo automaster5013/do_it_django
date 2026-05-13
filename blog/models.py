58c022f2f204498c05a5067f50e936d9af663f21
myeonghwi <automaster5013@gmail.com>
2026-05-13 15:33:25 +0900

    create blog app & single_pages app

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):      # 매직 메서드(__str__) : 특정객체를 문자열화!
+        return f'[{self.pk}] {self.title}'
