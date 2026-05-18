153f513b88306dd959e21e8fd76174c45760fd6a
myeonghwi <automaster5013@gmail.com>
2026-05-18 15:44:19 +0900

    357page, 태그 생성

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.shortcuts import render
 
 class PostList(ListView):
@@ -11,7 +11,6 @@ class PostList(ListView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
-
 
 def category_page(request, slug):
     if slug == 'no_category':
@@ -28,6 +27,21 @@ def category_page(request, slug):
         'category': category,
     })
 
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request, 
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects
+            .filter(category=None).count(),
+        } 
+    )
 
 class PostDetail(DetailView):
     model = Post
